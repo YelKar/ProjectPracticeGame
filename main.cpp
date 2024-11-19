e87ff59015d6b75d0fb4bbaba61681ef9e02c844
YelKar <karamyshev_e@1060.ru>
2024-11-19 18:10:52 +0300

    Init

--- main.cpp
+++ main.cpp
@@ -1,27 +1,11 @@
 #include "common/PixelImage.cpp"
 #include "assets/letters.cpp"
+#include "Game.h"
 
 sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!"/*, sf::Style::Fullscreen*/);
 
 int main() {
-
-    while (window.isOpen())
-    {
-        sf::Event event{};
-        while (window.pollEvent(event))
-        {
-            switch (event.type) {
-                case sf::Event::Closed:
-                    window.close();
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        window.clear();
-        drawPixelImage(window, LETTERS['A'], {20, 20}, 10);
-        window.display();
-    }
+    Game::init();
+    Game::loop(window);
     return 0;
 }
